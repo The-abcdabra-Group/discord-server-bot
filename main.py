cd667fe6d0b874627a9569b8373d6be0b8c8d797
abcdabra <abcdabra@outlook.com>
2022-10-23 13:34:43 -0500

    Enabled type checking

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from commands.economy import economy
 from services.antinuke import antinuke
 
 client = discord.Client(intents=discord.Intents.all())
-client.tree = app_commands.CommandTree(client)
+tree = app_commands.CommandTree(client)
 
 config = json.load(open("config.json"))
 
@@ -20,8 +20,8 @@ async def on_ready():
 
 
 async def commands():
-    await moderation(client, client.tree, config)
-    await economy(client, client.tree, config)
+    await moderation(client, tree, config)
+    await economy(client, tree, config)
 
     if config["commands"]["sync"] == False:
         print("Skipped syncing slash commands!")
@@ -30,16 +30,16 @@ async def commands():
     elif config["commands"]["sync"] == "ask":
         data = input("Would you like to sync slash commands with the discord API? (Y/N) ")
         if data == "Y" or data == "y":
-            await client.tree.sync(guild=discord.Object(id=config["commands"]["guild"]))
-            await client.tree.sync(guild=discord.Object(id=config["commands"]["beta_guild"]))
+            await tree.sync(guild=discord.Object(id=config["commands"]["guild"]))
+            await tree.sync(guild=discord.Object(id=config["commands"]["beta_guild"]))
             print("Synced slash commands!")
         else:
             print("Skipped syncing slash commands!")
             pass
 
     elif config["commands"]["sync"] == True:
-        await client.tree.sync(guild=discord.Object(id=config["commands"]["guild"]))
-        await client.tree.sync(guild=discord.Object(id=config["commands"]["beta_guild"]))
+        await tree.sync(guild=discord.Object(id=config["commands"]["guild"]))
+        await tree.sync(guild=discord.Object(id=config["commands"]["beta_guild"]))
         print("Synced slash commands!")
 
 
