c127ad4c9a0f74d8c83af225afc9c94c933ad51a
abcdabra <abcdabra@outlook.com>
2022-10-24 03:08:20 -0500

    Added additional functionality to link filter

--- services/automod.py
+++ services/automod.py
@@ -1,6 +1,7 @@
 import re
 import discord
 from datetime import timedelta
+from utils.punishments import Punishments
 
 async def automod(client: discord.Client, config):
     link_regex = r"((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*"
@@ -17,8 +18,11 @@ async def automod(client: discord.Client, config):
             return
 
         if re.search(link_regex, message.content) and config["automod"]["filter_links"] == True:
+            punishment = Punishments(client, message.author, config)
+
             await message.channel.send(f"<a:deny:1033973641030946826> {message.author.mention} Please refrain from sending links on the server!")
             await message.delete()
+            await punishment.warn("Sent unauthorized links in public chat", "Auto Moderator")
             return
 
         if re.search(profanity_regex, message.content, flags=re.IGNORECASE) and config["automod"]["filter_profanity"] == True:
