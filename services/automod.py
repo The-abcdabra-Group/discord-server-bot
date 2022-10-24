4f9210e8f2514ae28a472a3507fa062f97e57d7f
Dviih <dviih2s@gmail.com>
2022-10-24 18:42:54 -0300

    Simplified statement for check who sent message.

--- services/automod.py
+++ services/automod.py
@@ -10,12 +10,9 @@ async def automod(client: discord.Client, config):
 
     @client.event
     async def on_message(message: discord.Message):
-        staff_role = message.guild.get_role(config["roles"]["staff_role"])
-
-        if message.author.guild_permissions.administrator == True:
             return
 
-        if staff_role in message.author.roles:
+        if message.author.guild_permissions.administrator or message.guild.get_role(config["roles"]["staff_role"]) in message.author.roles:
             return
 
         if re.search(link_regex, message.content) and config["automod"]["filter_links"] == True:
