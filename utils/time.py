a189a4ab2dd71752f7cde17a7009ae6e068feae9
Dviih <dviih2s@gmail.com>
2022-10-24 18:46:44 -0300

    Made a function and used match instead of ifs.

--- utils/time.py
+++ utils/time.py
@@ -1,32 +1,15 @@
 from datetime import timedelta
 
-class Time:
-    """Represents a Time handler
-    This class is used to convert strings to timedelta and datetime/timedelta to string
-    """
-    def __init__(self):
-        pass
 
-    async def convert_time(self, data: str):
-        if data.endswith("d"):
-            time = int(data.removesuffix("d"))
-            pytime = timedelta(days=time)
-            return pytime
-
-        elif data.endswith("h"):
-            time = int(data.removesuffix("h"))
-            pytime = timedelta(hours=time)
-            return pytime
-
-        elif data.endswith("m"):
-            time = int(data.removesuffix("m"))
-            pytime = timedelta(minutes=time)
-            return pytime
-
-        elif data.endswith("s"):
-            time = int(data.removesuffix("s"))
-            pytime = timedelta(seconds=time)
-            return pytime
-
-        else:
+def convert_time(data: str):
+    match data.lower()[-1]:
+        case "d":
+            return timedelta(days=int(data[:-1]))
+        case "h":
+            return timedelta(hours=int(data[:-1]))
+        case "m":
+            return timedelta(minutes=int(data[:-1]))
+        case "s":
+            return timedelta(seconds=int(data[:-1]))
+        case _:
             raise ValueError("The time inputted is in an improper format!")
