e959cc353204ab77dae01e0bea782cfcc94d80d8
Dviih <dviih2s@gmail.com>
2022-10-24 18:44:48 -0300

    Simplified if statement.

--- services/verify.py
+++ services/verify.py
@@ -1,18 +1,14 @@
 import discord
 
+
 async def member_verification(client: discord.Client, config):
     @client.event
     async def on_interaction(interaction: discord.Interaction):
         if interaction.channel_id != config["verification"]["channel"]:
             return
-
         elif interaction.data["custom_id"] != config["verification"]["interaction_id"]:
             return
-            
         else:
-            verification_roles = config["verification"]["verification_roles"]
-            for data in verification_roles:
-                role = interaction.guild.get_role(data)
-                await interaction.user.add_roles(role)
-
+            for data in config["verification"]["verification_roles"]:
+                await interaction.user.add_roles(interaction.guild.get_role(data))
             await interaction.response.defer()
