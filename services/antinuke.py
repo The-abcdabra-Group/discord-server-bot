a7a2bb1d67256971652dd1885e1b8a94dbf702ad
Dviih <dviih2s@gmail.com>
2022-10-24 18:41:52 -0300

    Refactored mainly ifs and added one space.

--- services/antinuke.py
+++ services/antinuke.py
@@ -1,12 +1,15 @@
 import discord
 
+
 async def antinuke(client: discord.Client, config):
-    if config["antinuke"]["enabled"] == False:
+    if not config["antinuke"]["enabled"]:
         return
+
     # Reject all discord bots from joining the server
     @client.event
     async def on_member_join(member):
-        if member.bot == True:
-            await member.ban(reason = "abcdabra Anti Nuke Defense")
+        if member.bot:
             channel = await client.fetch_channel(config["logs_channel"])
+
+            await member.ban(reason="abcdabra Anti Nuke Defense")
             await channel.send(f"The server anti-nuke prevented `{member}` from joining")
