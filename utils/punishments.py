250bb02018772cc5bbda62bca5b668da7c3f2ec6
abcdabra <abcdabra@outlook.com>
2022-10-24 03:08:54 -0500

    Added staff channel logging to punishments.py class

--- utils/punishments.py
+++ utils/punishments.py
@@ -1,5 +1,5 @@
 import discord
-from utils.time import time
+from utils.time import convert_time
 from typing import Union
 
 class Punishments:
@@ -12,17 +12,24 @@ class Punishments:
         Represents a client connection that connects to Discord. This class is used to interact with the Discord WebSocket and API.
     member: :class:`discord.Member`
         Represents a Discord member to a Guild.
+    config: :json:
+        Represents the application configuration
     """
 
-    def __init__(self, client: discord.Client, member: discord.Member):
+    def __init__(self, client: discord.Client, member: discord.Member, config):
         self.client = client
         self.member = member
+        self.config = config
 
 
     async def warn(self, reason: str, author: Union[str, discord.Member]):
+        staff_logs = await self.client.fetch_channel(self.config["channels"]["staff_logs"])
+
         embed = discord.Embed(color=0xEF4444).set_author(name="Punishment Recieved").add_field(name="You have been warned", value=f"Reason: `{reason}` \n*Punishments are non-appealable*")
-        
+        logs_embed = discord.Embed(color=0xEF4444).set_author(name="Punishment Logs").add_field(name=f"Member was warned", value=f"{self.member.mention} **was warned** \nReason: `{reason}` \nPunished By: `{author}`")
+
+        await staff_logs.send(embed=logs_embed)
         try:
             await self.member.send(embed=embed)
         except:
-            pass
+            await staff_logs.send(f"<a:deny:1033973641030946826> Unable to notify {self.member.mention} of punishment")
