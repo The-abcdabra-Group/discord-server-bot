f1da674effd21d394941256d9cf0fd8b8be40a66
abcdabra <abcdabra@outlook.com>
2022-10-24 10:48:06 -0500

    converted time utility to Class format

--- utils/time.py
+++ utils/time.py
@@ -1,25 +1,32 @@
 from datetime import timedelta
 
-async def convert_time(data: str):
-    if data.endswith("d"):
-        time = int(data.removesuffix("d"))
-        pytime = timedelta(days=time)
-        return pytime
+class Time:
+    """Represents a Time handler
+    This class is used to convert strings to timedelta and datetime/timedelta to string
+    """
+    def __init__(self):
+        pass
 
-    elif data.endswith("h"):
-        time = int(data.removesuffix("h"))
-        pytime = timedelta(hours=time)
-        return pytime
+    async def convert_time(self, data: str):
+        if data.endswith("d"):
+            time = int(data.removesuffix("d"))
+            pytime = timedelta(days=time)
+            return pytime
 
-    elif data.endswith("m"):
-        time = int(data.removesuffix("m"))
-        pytime = timedelta(minutes=time)
-        return pytime
+        elif data.endswith("h"):
+            time = int(data.removesuffix("h"))
+            pytime = timedelta(hours=time)
+            return pytime
 
-    elif data.endswith("s"):
-        time = int(data.removesuffix("s"))
-        pytime = timedelta(seconds=time)
-        return pytime
+        elif data.endswith("m"):
+            time = int(data.removesuffix("m"))
+            pytime = timedelta(minutes=time)
+            return pytime
 
-    else:
-        raise ValueError("The time inputted is in an improper format!")
+        elif data.endswith("s"):
+            time = int(data.removesuffix("s"))
+            pytime = timedelta(seconds=time)
+            return pytime
+
+        else:
+            raise ValueError("The time inputted is in an improper format!")
