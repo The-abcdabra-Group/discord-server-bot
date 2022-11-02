c3e24cb88753c9e484b46a61583b4c2ef17b74b1
abcdabra <abcdabra@outlook.com>
2022-11-02 08:44:45 -0500

    Added profanity filter to the automoderator

--- services/automod.py
+++ services/automod.py
@@ -7,10 +7,11 @@ from utils.punishments import Punishments
 async def automod(client: discord.Client, config):
     link_regex = r"((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z]){2,6}([a-zA-Z0-9\.\&\/\?\:@\-_=#])*"
     profanity_regex = r"pussy|cum|nude|nudes|nudities|fuck me|dick|cock|boobs|vagina|orgy|orgi|horny|horni|fuck|fuck you"
+    ban_regex = r"nigger|nigga|retard|faggot"
 
     @client.event
     async def on_message(message: discord.Message):
-        if not isinstance(message.channel, discord.TextChannel):
+        if not isinstance(message.channel, discord.TextChannel) or not config["automod"]["enabled"]:
             return
 
         if message.author.guild_permissions.administrator or message.guild.get_role(config["roles"]["staff_role"]) in message.author.roles:
@@ -21,12 +22,23 @@ async def automod(client: discord.Client, config):
 
             await message.channel.send(f"<a:deny:1033973641030946826> {message.author.mention} Please refrain from sending links on the server!")
             await message.delete()
-            await punishment.warn("Sent unauthorized links in public chat", "Auto Moderator")
+            await punishment.warn("Sent unauthorized links in public chat` \nContents: `{message.content}", "Auto Moderator")
             return
 
         if config["automod"]["filter_profanity"] and re.search(profanity_regex, message.content, flags=re.IGNORECASE):
             punishment = Punishments(client, message.author, config)
             await message.channel.send(f"<a:deny:1033973641030946826> {message.author.mention} Please refrain from using profanity on the server!")
             await message.delete()
-            await punishment.mute("Sent profanity on the server", "5m", "Auto Moderator")
+            await punishment.mute("Sent profanity on the server` \nContents: `{message.content}", "5m", "Auto Moderator")
+            return
+
+        if config["automod"]["filter_profanity"] and re.search(ban_regex, message.content, flags=re.IGNORECASE):
+            punishment = Punishments(client, message.author, config)
+            await message.delete()
+            await punishment.ban(f"Used derogatory slurs and/or other blacklisted words` \nContents: `{message.content}", "Auto Moderator")
+            return
+        if config["automod"]["filter_everyone_ping"] and re.search(r"@everyone", message.content, flags=re.IGNORECASE):
+            punishment = Punishments(client, message.author, config)
+            await message.delete()
+            await punishment.ban(f"Attempted to ping @everyone` \nContents: `{message.content}", "Auto Moderator")
             return
