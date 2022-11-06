0c5b14a2b144f5e6c5501748399dd7e3265c115e
abcdabra <abcdabra@outlook.com>
2022-11-06 02:05:31 -0600

    Added working balance command

--- commands/economy.py
+++ commands/economy.py
@@ -1,8 +1,18 @@
 import discord
 from discord import app_commands
+from pymongo import MongoClient
+from random import randrange
 
+async def economy(client: discord.Client, tree: app_commands.CommandTree, mongodb: MongoClient, config):
 
-async def economy(client: discord.Client, tree: app_commands.CommandTree, config):
     @tree.command(name="balance", description="Returns the user's economy balance", guild=discord.Object(id=config["commands"]["beta_guild"]))
     async def balance(interaction: discord.Interaction):
-        await interaction.response.send_message("Your balance is `NULL` :coin:")
+        balance = mongodb["economy"]["balance"]
+
+        if balance.find_one({"_id": interaction.user.id}) == None:
+            balance.insert_one({"balance": 0, "_id": interaction.user.id})
+        
+        data = balance.find_one({"_id": interaction.user.id})
+        await interaction.response.send_message(f"Your balance is {data['balance']} :coin:")
+
+    
